8b994b988c31c7418a26974160da94fa1abc2414
haticesudepolat <hspolat0453@gmail.com>
2025-12-12 14:42:34 +0300

    Pom.xml duzenlendi ve JavaFX ayarlari yapildi

---
+++ src/main/java/org/project/Starter.java
@@ -0,0 +1,9 @@
+package org.project;
+
+public class Starter {
+    public static void main(String[] args) {
+        // MainApp senin asıl JavaFX sınıfının adıdır.
+        // Eğer sınıf adın farklıysa burayı güncelle.
+        MainApp.main(args);
+    }
+}
